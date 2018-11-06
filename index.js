2739f342fda65f0dbe5c6fafe413891a4322e876
VaheShaxpazyan <vahe-9231@mail.ru>
2018-11-06 18:29:35 +0400

    create app

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const cors = require('cors')
 
 const app = express()
 
-app.use('/',express.static('build'))
+app.use('/',express.static(__dirname + '/client/' +'build'))
 
 app.listen(process.env.PORT || 3001)
