60c25ec158bcf8a169c8aca4e27db8b7633e3e2b
VaheShaxpazyan <vahe-9231@mail.ru>
2018-11-10 22:37:44 +0400

    start project

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
 
 app.use('/',express.static(__dirname + '/client/' +'build'))
 
